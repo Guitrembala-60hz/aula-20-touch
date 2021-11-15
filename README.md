c112a33f892cddfc0050c0bec59291199536b607
Guitrembala-60hz <93789293+Guitrembala-60hz@users.noreply.github.com>
2021-11-15 20:58:36 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# aula-20-touch
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(800,400);
+  retFixo = createSprite(600,200,50,80);
+  retFixo.shapeColor = "green";
+  retFixo.debug = true;
+  retMovendo = createSprite(400,800,80,30);
+  retMovendo.shapeColor = "green";
+  retMovendo.debug = true;
+
+}
+
+function draw() {
+  background(255,255,255); 
+  
+  retMovendo.x = World.mouseX;
+  retMovendo.y = World.mouseY;
+
+  if(retMovendo.x - retFixo.x < retFixo.width/2 + retMovendo.width/2 &&
+    retFixo.x - retMovendo.x < retFixo.width/2 + retMovendo.width/2 &&
+    retMovendo.y - retFixo.y < retFixo.height/2 + retMovendo.height/2 &&
+    retFixo.y - retMovendo.y < retFixo.height/2 + retMovendo.height/2){
+    retFixo.shapeColor = "red";
+    retMovendo.shapeColor = "red";
+  }
+   else {
+    retFixo.shapeColor = "green";
+    retMovendo.shapeColor = "green";
+   }
+
+  console.log(retFixo.x - retMovendo.x);
+
+  drawSprites();
+}
